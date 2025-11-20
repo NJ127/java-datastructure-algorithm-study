3da66ae85989580ff1c66fcb7d6b8f45d643965e
NJ127 <3511423266@qq.com>
2025-11-20 11:02:28 +0800

    初始化提交：添加数据结构与算法项目代码

---
+++ src/com/nj127/dsa/arraylinked/array.java
@@ -0,0 +1,59 @@
+package com.nj127.dsa.arraylinked;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class array {
+    /* 随机访问元素 */
+    static int randomAccess(int[] nums) {
+        // 在区间 [0, nums.length) 中随机抽取一个数字
+
+        // 获取并返回随机元素
+
+        return 0;
+    }
+
+    /* 扩展数组长度 */
+    static int[] extend(int[] nums, int enlarge) {
+        // 初始化一个扩展长度后的数组
+
+        // 将原数组中的所有元素复制到新数组
+
+        // 返回扩展后的新数组
+        return null;
+    }
+
+    /* 在数组的索引 index 处插入元素 num */
+
+
+    /* 删除索引 index 处的元素 */
+
+
+    /* 遍历数组 */
+
+
+    /* 在数组中查找指定元素 */
+
+
+    /* Driver Code */
+    public static void main(String[] args) {
+        /* 初始化数组 */
+
+        /* 随机访问 */
+
+        /* 长度扩展 */
+
+
+        /* 插入元素 */
+
+
+        /* 删除元素 */
+
+
+        /* 遍历数组 */
+
+
+        /* 查找元素 */
+
+    }
+}
